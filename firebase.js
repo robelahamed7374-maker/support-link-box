2883af5048c3b6204af0f3482ea34d5f5200430d
robelahamed7374-maker <robelahamed7374@gmail.com>
2026-07-03 18:31:59 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -22,7 +22,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBf06m_Y9i8ieLhTrML1cJipAU52_aFebg",
   authDomain: "support-link-box.firebaseapp.com",
   projectId: "support-link-box",
-  storageBucket: "support-link-box.firebasestorage.app",
+  storageBucket: "support-link-box.appspot.com",
   messagingSenderId: "1067338452471",
   appId: "1:1067338452471:web:912353ac976dce6927b2f2"
 };
@@ -31,17 +31,3 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
-export {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  sendPasswordResetEmail,
-  signOut,
-  onAuthStateChanged,
-  doc,
-  setDoc,
-  getDoc,
-  updateDoc,
-  arrayUnion,
-  arrayRemove
-};
