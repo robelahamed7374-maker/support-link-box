7393e8352450be611ed9b381846bcbc6c9bf7f68
robelahamed7374-maker <robelahamed7374@gmail.com>
2026-07-05 12:43:50 +0600

    Refactor Firebase setup and remove Firestore usage
    Removed Firestore imports and updated Firebase configuration with placeholders.

--- firebase.js
+++ firebase.js
@@ -1,33 +1,23 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
 import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  sendPasswordResetEmail,
-  signOut,
-  onAuthStateChanged
+getAuth,
+createUserWithEmailAndPassword,
+signInWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
 
-import {
-  getFirestore,
-  doc,
-  setDoc,
-  getDoc,
-  updateDoc,
-  arrayUnion,
-  arrayRemove
-} from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
-
 const firebaseConfig = {
-  apiKey: "AIzaSyBf06m_Y9i8ieLhTrML1cJipAU52_aFebg",
-  authDomain: "support-link-box.firebaseapp.com",
-  projectId: "support-link-box",
-  storageBucket: "support-link-box.appspot.com",
-  messagingSenderId: "1067338452471",
-  appId: "1:1067338452471:web:912353ac976dce6927b2f2"
+apiKey: "YOUR_API_KEY",
+authDomain: "YOUR_PROJECT.firebaseapp.com",
+projectId: "YOUR_PROJECT_ID",
+storageBucket: "YOUR_PROJECT.appspot.com",
+messagingSenderId: "XXXX",
+appId: "XXXX"
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
-export const db = getFirestore(app);
+
+export {
+createUserWithEmailAndPassword,
+signInWithEmailAndPassword
+};
